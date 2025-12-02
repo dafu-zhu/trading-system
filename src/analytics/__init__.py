ebca45b2069ca4ac622d7745ce269e7f2418d819
Dafu <dafuzhu@uchicago.edu>
2025-12-01 22:23:18 -0600

    complete reporting

---
+++ src/analytics/__init__.py
@@ -0,0 +1,12 @@
+from analytics.metrics import PerformanceMetrics, format_metrics
+from analytics.analyzer import BacktestAnalyzer
+from analytics.visualizer import BacktestVisualizer
+from analytics.report import MarkdownReportGenerator
+
+__all__ = [
+    'PerformanceMetrics',
+    'format_metrics',
+    'BacktestAnalyzer',
+    'BacktestVisualizer',
+    'MarkdownReportGenerator',
+]
--- src/backtester/__init__.py
+++ src/backtester/__init__.py
@@ -1,4 +1,31 @@
 from backtester.execution import ExecutionEngine
 from backtester.position_sizer import PositionSizer
+from backtester.recorder import BacktestRecorder
+from backtester.costs import (
+    TransactionCostCalculator,
+    CommissionModel,
+    SlippageModel,
+    FixedCommission,
+    PerShareCommission,
+    PercentageCommission,
+    TieredCommission,
+    FixedSlippage,
+    PercentageSlippage,
+    VolumeSlippage,
+)
 
-__all__ = ['ExecutionEngine', 'PositionSizer']
+__all__ = [
+    'ExecutionEngine',
+    'PositionSizer',
+    'BacktestRecorder',
+    'TransactionCostCalculator',
+    'CommissionModel',
+    'SlippageModel',
+    'FixedCommission',
+    'PerShareCommission',
+    'PercentageCommission',
+    'TieredCommission',
+    'FixedSlippage',
+    'PercentageSlippage',
+    'VolumeSlippage',
+]
